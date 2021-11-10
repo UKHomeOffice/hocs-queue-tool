59fa5342753572dfb065663dd4ba8af4d7abccd6
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-10 14:57:06 +0000

    Correct run.sh jar name

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NAME=${NAME:-audit}
+NAME=${NAME:-queue}
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
 exec java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
