92d011c4cb534efa7ed2be49b22501c10e9a651c
Liam MacPherson <liammacpherson1996@gmail.com>
2021-11-10 15:58:04 +0000

    Update setup-sqs script
    Change the setup-sqs script to be able to run within the docker pipeline
    . Thi will support both docker-compose.yml runs and pipeline. This
    change also introduces an health check for LocalStack to ensure its able
     to accept the request. Instead of setting the queues attributes
     separately, this brings it inline with the queue generation.

--- config/localstack/setup-sqs.sh
+++ config/localstack/setup-sqs.sh
@@ -1,32 +1,25 @@
 #!/usr/bin/env bash
-set -e
-export TERM=ansi
+set -euo
+
 export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
-export PAGER=
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name search-sqs
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name search-dlq
+until curl http://localstack:4566/health --silent | grep -q "running"; do
+   sleep 5
+   echo "Waiting for LocalStack to be ready..."
+done
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name audit-sqs
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name audit-dlq
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name search-dlq --attributes '{"VisibilityTimeout":"1"}'
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name search-sqs --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:search-dlq\",\"maxReceiveCount\":1}"}'
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name document-sqs
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name document-dlq
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name audit-dlq --attributes '{"VisibilityTimeout":"1"}'
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name audit-sqs --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:audit-dlq\",\"maxReceiveCount\":1}"}'
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name notify-sqs
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name notify-dlq
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name document-dlq --attributes '{"VisibilityTimeout":"1"}'
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name document-sqs --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:document-dlq\",\"maxReceiveCount\":1}"}'
 
-
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/search-sqs" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:search-dlq\"}"}'
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/audit-sqs" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:audit-dlq\"}"}'
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/document-sqs" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:document-dlq\"}"}'
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/notify-sqs" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:notify-dlq\"}"}'
-
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/search-dlq" --attributes '{"VisibilityTimeout":"1"}'
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/audit-dlq" --attributes '{"VisibilityTimeout":"1"}'
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/document-dlq" --attributes '{"VisibilityTimeout":"1"}'
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/notify-dlq" --attributes '{"VisibilityTimeout":"1"}'
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name notify-dlq --attributes '{"VisibilityTimeout":"1"}'
+aws --endpoint-url=http://localstack:4576 sqs create-queue --queue-name notify-sqs --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:notify-dlq\",\"maxReceiveCount\":1}"}'
 
 echo Queues are Ready
